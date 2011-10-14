26ed3c5819cf63d786466f9207d39b5f53086a50
Christian Muehlethaler <chmuehlethaler@freesurf.ch>
2011-10-14 21:42:37 +0200

    drag-n-drop refined

--- app/assets/javascripts/mastermind.js
+++ app/assets/javascripts/mastermind.js
@@ -9,20 +9,40 @@ $(document).ready(function() {
         helper: 'clone',
         opacity: 0.35
     });
+    $(".target").draggable({
+        helper: 'clone',
+        opacity: 0.35,
+     //   (self).css("background-color": white)
+        stop: function(event, ui){
+            var draggable = $(this);
+            draggable.removeAttr("style");
+           // alert(draggable[0].id);
+            //draggable.css("background-color", "white");
+            var index = draggable[0].id.match(/\d+/);
+            // log(index)
+
+             $("#guess_"+index).val("");
+
+        }
+    });
     $(".target").droppable(
         {
             drop: function(event, ui) {
-                var dropped = ui.draggable[0];
                 var droppedOn = $(this);
-                droppedOn.css({"background": dropped.id});
-                log(droppedOn[0].id);
+                var dropped = ui.draggable[0];
+                var background_color = dropped.getAttribute('color');
+                droppedOn.attr('color', background_color);
+                droppedOn.css({"background-color": background_color});
                 var index = droppedOn[0].id.match(/\d+/);
                // log(index)
 
-                $("#guess_"+index).val(dropped.id);
+                $("#guess_"+index).val(background_color);
 
-            }
+            },
+            hoverClass: 'drophover'
+
         }
+
 
     );
 
