803bdaa508e565039330bb20308f5629d98a4982
Christian Muehlethaler <chmuehlethaler@freesurf.ch>
2011-12-15 22:19:35 +0100

    created a spec for statemachine_helper_spec

--- app/helpers/statemachine_helper.rb
+++ app/helpers/statemachine_helper.rb
@@ -5,13 +5,10 @@ module StatemachineHelper
   end
 
   def links
-
     links = []
     @plane.state_events.each do |event|
-       #links << {name: event.to_s, path: send("statemachine_#{event.to_s}_path")}
        links << {name: event.to_s, path: statemachine_path(event: event)}
     end
-    p links
     links
   end
 end
--- spec/helpers/statemachine_helper_spec.rb
+++ spec/helpers/statemachine_helper_spec.rb
@@ -1,15 +1,19 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the StatemachineHelper. For example:
-#
-# describe StatemachineHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
+
 describe StatemachineHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @plane = double "post"
+    @plane.stub(:state_events){%w[a b c]}
+  end
+  it "creates links" do
+    links = helper.links
+    links.size.should == 3
+    p links
+    links[0].should be_an_instance_of Hash
+    links[0][:name].should == "a"
+    links[0][:path].should == "/statemachine?event=a"
+
+  end
+
 end
